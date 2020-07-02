662f7264a978507001e468373b32bd226116d567
sanjay-shah <sanjay_y2k@yahoo.com>
2020-07-02 13:41:30 -0700

    re-indented

--- cronRiskUpdater.js
+++ cronRiskUpdater.js
@@ -11,17 +11,14 @@ async function cron()  {
     	'https://testdrive656.aquasec.com/api/v1/login', 
     	login
     )
-    console.log('fetched access token.')
-    console.log('attempting to fetch images...')
+console.log('fetched access token.')
+console.log('attempting to fetch images...')
 const response = 
 	await axios.get('https://testdrive656.aquasec.com/api/v2/images?page=1&include_totals=true&order_by=name&page_size=1000', { 
 		headers: { Authorization: "Bearer " + auth.data.token } 
   })
-    console.log('fetched images.')
-//console.log(response.data.result)
-	/*for (let i=0; i<response.data.result.length; i++) {
-		
-	}*/
+console.log('fetched images.')
+
 const tsResponse = 
 	await axios.post(
         	'http://localhost:3000/risks',
