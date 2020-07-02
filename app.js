1c2c0dc58c87255be2761699676c3222c1e20137
sanjay-shah <sanjay_y2k@yahoo.com>
2020-07-02 18:13:30 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,109 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose')
+
+const app = express();
+const port = 3000;
+
+mongoose.connect(
+	'mongodb://localhost:27017/test', { 
+		useNewUrlParser: true, 
+		useUnifiedTopology: true 
+	}
+)
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'))
+
+const RiskModel = mongoose.model('risks', { 
+	imageName: String,
+	repoName: String,
+	namespace: String,
+	clusterId: String,
+	critVulns: Number,
+	highVulns: Number,
+	medVulns: Number,
+	lowVulns: Number,
+	totalVulns: Number,
+	ackVulns: Number,
+	//timestamp is only added for demo
+	//in production use _id to derive timestamp
+	timestamp: {
+		type: Date,
+		default: new Date
+	}
+})
+// Allow CORS
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// This function returns an ObjectId embedded with a given datetime
+// Accepts both Date object and string input
+
+function objectIdWithTimestamp(timestamp) {
+    // Convert string date to Date object (otherwise assume timestamp is a date)
+    if (typeof(timestamp) == 'string') {
+        timestamp = new Date(timestamp);
+    }
+    // Convert date object to hex seconds since Unix epoch
+    const hexSeconds = Math.floor(timestamp/1000).toString(16);
+    // Create an ObjectId with that hex timestamp
+    const constructedObjectId = mongoose.Types.ObjectId(hexSeconds + "0000000000000000");
+    return constructedObjectId
+}
+
+app.get('/risks', (req, res) => {
+    console.log('GET ' + req.originalUrl)
+    if (!req.query.from 
+	    || !req.query.to 
+	    ) {
+	    return res.status(400).send({
+   		message: 'from,to,imageName and repoNsame required in query str'
+	    }) 
+    }
+    RiskModel.find({
+	    _id:{
+		    $lte: objectIdWithTimestamp(req.query.to),
+		    $gte: objectIdWithTimestamp(req.query.from)
+	    },
+	    imageName: req.query.imageName
+    }, function (err, risks) {
+	  if (err) return console.error(err)
+	  //run this within a loop to send timestamps explicitly
+	  //risks[i].timestamp = risk[i]._id.getTimestamp()
+	  //Timestamps can be easily extracted from objectId on the client side:
+	  //objectId='5efba3f6508ccd2568e8f0e8'
+	  //time=new Date(parseInt(objectId.substring(0, 8), 16) * 1000)
+	  res.send(risks)
+    })
+})
+
+app.post('/risks', (req, res) => {
+        console.log('inside POST /risks')
+	//console.log(req.body)i
+	let riskItem = null
+	for (let i=0;i<req.body.length; i++) {
+		riskItem = new RiskModel({
+                  imageName: req.body[i].name,
+                  repoName: req.body[i].registry,
+                  namespace: "_blank",
+                  clusterId: "no-cluster-id",
+                  critVulns: req.body[i].crit_vulns,
+                  highVulns: req.body[i].high_vulns,
+                  medVulns: req.body[i].med_vulns,
+                  lowVulns: req.body[i].low_vulns,
+                  totalVulns: req.body[i].vulns_found,
+                  ackVulns: 0
+        	})
+
+		riskItem.save().then(() => console.log(riskItem))
+	}
+        res.send(req.body.length + ' images saved')
+})
+
+app.listen(port, () => console.log(`Risk timeseries API server listening on port ${port}`));
+
---
+++ cronRiskUpdater.js
@@ -0,0 +1,31 @@
+const axios = require('axios')
+
+const login = {
+  id: 'administrator',
+  password: 'NzgxYzll'
+}
+let auth = {}
+async function cron()  {
+    console.log('attempting to fetch access token...')
+    auth = await axios.post(
+    	'https://testdrive656.aquasec.com/api/v1/login', 
+    	login
+    )
+    console.log('fetched access token.')
+    console.log('attempting to fetch images...')
+const response = 
+	await axios.get('https://testdrive656.aquasec.com/api/v2/images?page=1&include_totals=true&order_by=name&page_size=1000', { 
+		headers: { Authorization: "Bearer " + auth.data.token } 
+  })
+    console.log('fetched images.')
+//console.log(response.data.result)
+	/*for (let i=0; i<response.data.result.length; i++) {
+		
+	}*/
+const tsResponse = 
+	await axios.post(
+        	'http://localhost:3000/risks',
+        	response.data.result
+    	)
+}
+cron()
